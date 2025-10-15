780b8f1f80b6bc3f230325ee979e86be8720a82e
spacezorro <spacezorro@gmail.com>
2025-10-15 12:51:15 -0700

    Remove logging for local registry unless LOG_LEVEL>=DEBUG

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,15 @@
 
 if [ "$REGISTRY_URL" = "http://localhost:5000" ]
 then
-  # Sending registry stuff to stderr for docker log filteringability
-  registry serve /etc/docker/registry/config.yml >/dev/stderr &
+  case "$LOG_LEVEL" in
+    DEBUG|TRACE) out=stderr ;;
+    *) out=null ;;
+  esac
+  registry serve /etc/docker/registry/config.yml >/dev/$out 2>&1 &
   REGPID=$!
   python /main.py
   kill $REGPID
-  registry garbage-collect /etc/docker/registry/config.yml >/dev/stderr
+  registry garbage-collect /etc/docker/registry/config.yml >/dev/$out 2>&1
 else
   python /main.py
 fi
